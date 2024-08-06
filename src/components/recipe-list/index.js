ec5b4c499ee4db5e8d5f583da03fd4e4fbe56689
Shahzad Ali <shahzad.ali41410@gmail.com>
2024-08-06 15:22:11 +0500

    Changed favicon and Fixed key prop warning

--- src/components/recipe-list/index.js
+++ src/components/recipe-list/index.js
@@ -11,7 +11,7 @@ export default function RecipeList({ recipeList }) {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {recipeList && recipeList.length > 0
             ? recipeList.map((recipe) => (
-                <Link href={`/recipe-list/${recipe.id}`}>
+                <Link key={recipe.id} href={`/recipe-list/${recipe.id}`}>
                   <Card>
                     <CardContent className="bg-white rounded-md overflow-hidden shadow-md cursor-pointer hover:scale-[1.1] transition-all">
                       <div className="w-full aspect-w-16 aspect-h-8 lg:h-80">
